ae4cad1a003122194f3b503e527df11584f8119e
EugeLynx <gehr.yaz@gmail.com>
2023-07-18 12:32:22 +0500

    Added temperature display in usre's local time

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 async function getForecast() {
     const result = await fetch("https://api.open-meteo.com/v1/forecast?latitude=43.2567&longitude=76.9286&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=auto");
     const json = await result.json();
-    const currentHours = new Date().getHours();
-    // const currentMinutes = new Date().getMinutes();
-
-        // Доделать перенос часов, дня, месяца, года
-    // const timeDiff = parseInt(json.timezone_abbreviation);
-    // let currentLocalHours = currentUTCHours + timeDiff;
-    // let currentLocalYear = new Date().getFullYear();
-    // let currentLocalMonth = new Date().getMonth();
-    // let currentLocalDay = new Date().getDay();
-    // if (currentLocalHours >= 24) {
-    //     currentLocalHours -= 24;
-    //     currentLocalDay.setDay(currentLocalDay + 1);
-    //     if 
-    // }
-
-    const currentYear = new Date().getFullYear();
-    let currentMonth = new Date().getMonth();
-    let currentDay = new Date().getDate();
-    currentMonth = (currentMonth + 1).toString().padStart(2, '0');
-    currentDay = currentDay.toString().padStart(2, '0');
-    const currentDate = 
-        `${currentYear}-${currentMonth}-${currentDay}T${currentHours}:00`;    
     
+    const utcOffsetHours = parseInt(json.timezone_abbreviation);
+    // const currentLocalHours = new Date().getHours();
+    // const currentLocalMinutes = new Date().getMinutes();
+    // const currentLocalYear = new Date().getFullYear();
+    // let currentLocalMonth = new Date().getMonth();
+    // let currentLocalDay = new Date().getDate();
+    // currentLocalMonth = (currentLocalMonth + 1).toString().padStart(2, '0');
+    // currentLocalDay = currentLocalDay.toString().padStart(2, '0');
+    // const currentLocalDateZeroMinutes = 
+    //     `${currentLocalYear}-${currentLocalMonth}-${currentLocalDay}T${currentLocalHours}:00`;
+    let currentLocalDateZeroMinutes = new Date();
+    currentLocalDateZeroMinutes.setMinutes(0);
+    currentLocalDateZeroMinutes.setSeconds(0);
+    currentLocalDateZeroMinutes.setMilliseconds(0);
+
+    let localDateTemp = [[], (json.hourly.temperature_2m)];
+    const targetDate = new Date(json.hourly.time[0]);
+    const targetUTCDate = new Date(targetDate.setHours(targetDate.getHours() - utcOffsetHours));
+    const localTimezoneOffset = new Date().getTimezoneOffset();
+    let targetLocalDate = new Date (targetUTCDate.setMinutes(targetUTCDate.getMinutes() - localTimezoneOffset));
+
+    for (let i = 0; i < (json.hourly.time).length; i++) {
+        localDateTemp[0][i] = targetLocalDate;
+        targetLocalDate = structuredClone(targetLocalDate);
+        targetLocalDate.setHours(targetLocalDate.getHours() + 1);    
+    };
+
     let currentTemperature = '';
-    for (let i = 0; i <= (json.hourly.time).length; i++) {
-        if (currentDate === json.hourly.time[i]) {
-            currentTemperature = json.hourly.temperature_2m[i];
+
+    for (let j = 0; j < (localDateTemp[0]).length; j++) {
+        console.log(currentLocalDateZeroMinutes);
+        console.log(localDateTemp[0][j]);
+        if (currentLocalDateZeroMinutes.getTime() === localDateTemp[0][j].getTime()) {
+            currentTemperature = localDateTemp[1][j];
         }
     }
-    
+ 
+    console.log(currentTemperature);
+
+
     document.getElementById('current').innerText = `${parseInt(currentTemperature)}°`
 
 // Добавить макс мин
-
-
-    console.log(currentDate);
-    console.log(currentTemperature);
 }
 getForecast();
